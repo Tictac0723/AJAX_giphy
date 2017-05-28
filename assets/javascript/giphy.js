0532fc441892cfedfc6c52d6d76f8c019033e69b
Tictac0723 <taylorclark0723@gmail.com>
2017-05-28 17:23:50 -0500

    it's alive!

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,23 +1,34 @@
-var topics = ["Bob's Burgers", "Silicon Valley", "Pretty Little Liars", "Sense 8", "Handmaid's Tale", "Fresh Prince of Belaire", "Game of Thrones", "Criminal Minds", "Friends", "Shameless", "The Office", "Riverdale", "The Unbreakable Kimmy Schmidt", "Sons of Anarchy", "Last Week Tonight", "Black Mirror", "New Girl"];
+var topics = ["Bob's Burgers", "Silicon Valley", "Pretty Little Liars", "Sense 8", "Handmaid's Tale", "Fresh Prince of Belaire", "Game of Thrones", "Friends", "Shameless", "Riverdale", "The Unbreakable Kimmy Schmidt"];
 var image = "";
+var ratings = "";
 $(document).ready(function() {
 
+
     for (var i = 0; i < topics.length; i++) {
-        var button = '<button class="buttons">' + topics[i] + '</button>';
+        var button = '<button type="button" class="btn btn-default buttons">' + topics[i] + '</button>';
         $('#topics').append(button);
     }
 
     $("#topics").on("click", ".buttons", function() {
-    	$("#images").empty();
+        $("#output").empty();
         $.ajax({
             url: "http://api.giphy.com/v1/gifs/search?q=" + $(this).html() + "&api_key=dc6zaTOxFJmzC&limit=10",
             method: "GET"
         }).done(function(response) {
+            console.log(response);
             for (var i = 0; i < response.data.length; i++) {
-                $("#images").prepend('<img src="' + response.data[i].images["original"].url + '">');
+                $("#output").prepend('<div class="row rating">' + "Rated: " + response.data[i].rating +  '</div> <div class="row images"><img src="' + response.data[i].images["original"].url + '"></div>' + "<hr>");
             }
-        })
+        });
+
     });
 
+    $("#newTopicButton").on("click", function(event) {
+        event.preventDefault();
+        var topic = $("#newTopic").val();
+        topics.push(topic);
+        var button = '<button type="button" class="btn btn-default buttons">' + topic + '</button>';
+        $('#topics').append(button);
+    });
 
 });
